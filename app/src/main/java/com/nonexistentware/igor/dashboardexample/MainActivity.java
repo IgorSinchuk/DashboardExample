778e19004e4fb60428d6b85b07031e70a958a583
Igor <igor@Igors-iMac.local>
2017-03-30 23:51:57 +0300

    Dashboard
    Android Example: Material Design Dashboard UI Design

---
+++ app/src/main/java/com/nonexistentware/igor/dashboardexample/MainActivity.java
@@ -0,0 +1,43 @@
+package com.nonexistentware.igor.dashboardexample;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ProgressBar myprogressBar;
+    TextView progressingTextView;
+    Handler progressHandler = new Handler();
+    int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myprogressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressingTextView = (TextView) findViewById(R.id.progress_circle_text);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (i < 100) {
+                    i += 2;
+                    progressHandler.post(new Runnable() {
+                        public void run() {
+                            myprogressBar.setProgress(i);
+                            progressingTextView.setText("" + i + " %");
+                        }
+                    });
+                    try {
+                        Thread.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+}
